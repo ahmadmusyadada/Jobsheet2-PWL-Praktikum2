3fd9ed107ce8f29dba9a1dea89e0d5a956d1bb2d
ahmadmusyadada <ahmadmusyadadaminullah@gmail.com>
2019-02-13 19:27:36 +0700

    Update main

---
+++ ts-hello/main.js
@@ -0,0 +1,25 @@
+var log = function pesan() {
+    console.log(pesan);
+};
+var doLog = function (pesan) {
+    console.log(pesan);
+};
+var doLog1 = function (pesan) { return console.log(pesan); };
+// let pesan;
+// pesan = 'abc';
+// let percobaan = (<string>pesan).endsWith('c');
+// let alternative = (pesan as string).endsWith('c');
+// let count = 5;
+// count = 'a';
+// function doSomething(){
+//     for (let i = 0; i < 5; i++){
+//         console.log(i);
+//     }
+//     console.log('Finally: ' + i);
+// }
+// doSomething();
+// function log(message){
+//     console.log(message);
+// }
+// let message = 'Hello World';
+// log(message);
---
+++ ts-hello/main.ts
@@ -0,0 +1,37 @@
+let log = function pesan(){
+    console.log(pesan);
+}
+
+let doLog = (pesan) => {
+    console.log(pesan);
+}
+
+let doLog1 = (pesan) => console.log(pesan);
+
+
+// let pesan;
+// pesan = 'abc';
+// let percobaan = (<string>pesan).endsWith('c');
+// let alternative = (pesan as string).endsWith('c');
+
+
+// let count = 5;
+// count = 'a';
+
+
+// function doSomething(){
+//     for (let i = 0; i < 5; i++){
+//         console.log(i);
+//     }
+//     console.log('Finally: ' + i);
+// }
+// doSomething();
+
+
+// function log(message){
+//     console.log(message);
+// }
+
+// let message = 'Hello World';
+
+// log(message);
